b8fae7ef4d1f5b00dd7b4ee321dc65e2e212f928
iyadxxadjeroud <ayad.adjeroud@student.umc.edu.dz>
2025-05-08 18:03:34 +0100

    Your commit message

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,11 +1,6 @@
 module.exports = {
   hooks: {
     readPackageJson(pkg) {
-      if (pkg.name === 'bcrypt') {
-        pkg.scripts = pkg.scripts || {};
-        // Force a rebuild of bcrypt using node-gyp
-        pkg.scripts.postinstall = 'node-gyp rebuild';
-      }
       return pkg;
     },
   },
